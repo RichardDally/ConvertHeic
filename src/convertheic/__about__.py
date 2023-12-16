95ff9eab3e73208582904985fdd991843b1390ff
Richard Dally <r.dally@protonmail.com>
2023-12-16 10:34:16 +0100

    Kickoff

---
+++ src/convertheic/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2023-present Richard Dally <r.dally@protonmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = "0.0.1"
---
+++ tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2023-present Richard Dally <r.dally@protonmail.com>
+#
+# SPDX-License-Identifier: MIT
